48a94045d8a30cf9eabed234f8498055d79edfe9
daic0r <daic0r.dev@gmail.com>
2024-03-30 01:49:22 +0100

    Implemented event loop

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,15 +1,7 @@
-use std::net::{TcpListener,SocketAddr,TcpStream};
 use std::io::{Error, Read, Write};
+use std::net::{SocketAddr, TcpListener, TcpStream};
 
 use redis::*;
-
-fn do_something(mut client: (TcpStream, SocketAddr)) {
-    let mut buf: [u8; 128] = [0; 128];
-    let n = client.0.read(&mut buf).unwrap();
-    println!("Read {} bytes", n);
-    println!("Client says {}", String::from_utf8_lossy(&buf));
-    client.0.write(b"world").unwrap();
-}
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:1234");
@@ -18,38 +10,49 @@ fn main() {
         return;
     }
     let listener = listener.ok().unwrap();
-    listener.set_nonblocking(true).expect("Cannot set non-blocking");
+    listener
+        .set_nonblocking(true)
+        .expect("Cannot set non-blocking");
 
     let mut connections = vec![];
 
     loop {
         match listener.accept() {
             Ok((client, addr)) => {
-                client.set_nonblocking(true).expect("Couldn't set non-blocking mode on accepted connection");
+                client
+                    .set_nonblocking(true)
+                    .expect("Couldn't set non-blocking mode on accepted connection");
                 println!("Got a connection from {}", addr);
-                let conn = Connection{
+                let conn = Connection {
                     fd: client,
                     state: ConnectionState::StateReq,
                     rbuf_size: 0,
                     rbuf: [0; 4 + MAX_MSG],
                     wbuf_size: 0,
                     wbuf_sent: 0,
-                    wbuf: [0; 4 + MAX_MSG]
+                    wbuf: [0; 4 + MAX_MSG],
                 };
                 connections.push(conn);
-            },
-            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {},
-            Err(e) => eprintln!("Error accepting connection: {}", e)
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {}
+            Err(e) => eprintln!("Error accepting connection: {}", e),
         }
-        for client in &connections {
+        for client in &mut connections {
             match client.state {
                 ConnectionState::StateReq => {
+                    client.state_req();
                 },
                 ConnectionState::StateRes => {
-                },
+                    client.state_res();
+                }
                 _ => {}
             }
         }
-        connections.retain(|e| e.state != ConnectionState::StateEnd);
+        connections.retain(|e| { 
+            if e.state == ConnectionState::StateEnd {
+                println!("Client disconnected");
+            }
+            e.state != ConnectionState::StateEnd
+        });
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,127 @@ pub struct Connection {
     pub wbuf: [u8; 4 + MAX_MSG]
 }
 
+impl Connection {
+    pub fn state_req(&mut self) {
+        while self.try_fill_buffer() {
+            println!("state_req looping");
+        }
+    }
+
+    fn try_fill_buffer(&mut self) -> bool {
+        assert!(self.rbuf_size < self.rbuf.len());
+
+        let mut rv = 0usize;
+        loop {
+            match self.fd.read(&mut self.rbuf[self.rbuf_size..]) {
+                Ok(n) => {
+                    rv = n;
+                    break;
+                },
+                Err(ref e) if e.kind() == std::io::ErrorKind::Interrupted => {},
+                Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                    return false;
+                },
+                Err(_) => {
+                    eprintln!("read() error");
+                    self.state = ConnectionState::StateEnd;
+                    return false;
+                }
+            }
+        }
+
+        if rv == 0 {
+            if self.rbuf_size > 0 {
+                eprintln!("Unexpected EOF");
+            } else {
+                eprintln!("EOF");
+            }
+            self.state = ConnectionState::StateEnd;
+            return false;
+        }
+
+        self.rbuf_size += rv;
+        assert!(self.rbuf_size <= self.rbuf.len());
+
+        while self.try_one_request() {}
+
+        self.state == ConnectionState::StateReq
+    }
+
+    fn try_one_request(&mut self) -> bool {
+        if self.rbuf_size < 4 {
+            return false;
+        }
+
+        let len = u32::from_le_bytes(self.rbuf[0..4].try_into().expect("need 4-byte array")) as usize;
+        if len > MAX_MSG {
+            eprintln!("Message too long");
+            self.state = ConnectionState::StateEnd;
+            return false;
+        }
+        if 4 + len > self.rbuf_size {
+            return false;
+        }
+
+        let msg = String::from_utf8_lossy(&self.rbuf[4..]);
+        println!("Client says: {}", msg);
+
+        self.wbuf[..4].copy_from_slice((len as u32).to_le_bytes().as_slice());
+        self.wbuf[4..4 + len].copy_from_slice(&self.rbuf[4..4 + len]);
+        self.wbuf_size = 4 + len;
+
+        let remain = self.rbuf_size - 4 - len;
+        if remain > 0 {
+            self.rbuf.copy_within(4 + len.., 0);
+        }
+        self.rbuf_size = remain;
+        self.state = ConnectionState::StateRes;
+        self.state_res();
+
+        self.state == ConnectionState::StateReq
+    }
+
+    pub fn state_res(&mut self) {
+        while self.try_flush_buffer() {
+            println!("state_res looping");
+        }
+    }
+
+    fn try_flush_buffer(&mut self) -> bool {
+        let mut rv = 0usize;
+        loop {
+            let remain = self.wbuf_size - self.wbuf_sent;
+            match self.fd.write(&mut self.wbuf[self.wbuf_sent..self.wbuf_sent+remain]) {
+                Ok(n) => {
+                    rv = n;
+                    break;
+                },
+                Err(ref e) if e.kind() == std::io::ErrorKind::Interrupted => {},
+                Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                    return false;
+                },
+                Err(_) => {
+                    eprintln!("write() error");
+                    self.state = ConnectionState::StateEnd;
+                    return false;
+                }
+            }
+        }
+
+        self.wbuf_sent += rv;
+        assert!(self.wbuf_sent <= self.wbuf_size);
+
+        if self.wbuf_sent == self.wbuf_size {
+            self.state = ConnectionState::StateReq;
+            self.wbuf_sent = 0;
+            self.wbuf_size = 0;
+            return false;
+        }
+
+        true
+    }
+}
+
 pub fn read_full(stream: &mut TcpStream, buf: &mut [u8], n: usize) -> bool {
     let mut bytes_left = Some(n);
     let mut offset = 0;
